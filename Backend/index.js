1636ee2b87ef53f28901b4222724e30cda04b164
Yogesh Pote <yogpote035@gmail.com>
2025-08-15 19:43:38 +0530

    feat: Update CORS configuration to specify allowed origins

--- Backend/index.js
+++ Backend/index.js
@@ -8,9 +8,23 @@ if (process.env.NODE_ENV !== "production") {
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://click2-hire-remote-job-platform-click2hires-projects.vercel.app",
+  "https://click2-hire-remote-job-platform.vercel.app",
+  "https://click2-hire-remote-job-platform-git-main-click2hires-projects.vercel.app",
+  "https://click2-hire-remote-job-platform-r0iq7z4zu-click2hires-projects.vercel.app",
+];
+
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN || "*",
-  methods: ["GET", "HEAD", "POST", "PUT", "DELETE","OPTIONS"],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
