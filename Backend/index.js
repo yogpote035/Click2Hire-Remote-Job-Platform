2bd45bf4ff8cfe470bd89de521793e899203e931
Yogesh Pote <yogpote035@gmail.com>
2025-08-15 20:32:58 +0530

    feat: Remove allowedHeaders from CORS options

--- Backend/index.js
+++ Backend/index.js
@@ -27,7 +27,6 @@ const corsOptions = {
     }
   },
   methods: ["GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
 
