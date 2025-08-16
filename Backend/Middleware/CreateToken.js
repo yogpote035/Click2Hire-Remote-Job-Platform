2accac89e34588edcf584c94e177e98e86487e5a
Yogesh Pote <yogpote035@gmail.com>
2025-08-16 20:02:13 +0530

    Updating in token

--- Backend/Middleware/CreateToken.js
+++ Backend/Middleware/CreateToken.js
@@ -1,27 +1,27 @@
 const { EncryptJWT } = require("jose");
 const encoder = new TextEncoder();
 
-async function CreateToken(userId, role, email, name) {
-  const raw = process.env.SECRET_KEY.slice(0, 32); // Ensure 32 bytes for A256GCM(Algorithm for encryption)
+async function CreateToken(userId, role, email, fullName) {
+  console.log("call in Create Token ");
+  const raw = process.env.SECRET_KEY.slice(0, 32); // must be the same as verify
   const secret = encoder.encode(raw);
 
-  console.log("Call Receive in JWE");
-
-  // Payload with all required fields
   const payload = {
-    userId,
-    role,
-    email,
-    name,
+    userId: userId.toString(),
+    role: role,
+    email: email,
+    name: fullName,
   };
+
+  console.log("payload from Create token : ", payload);
 
   const jwe = await new EncryptJWT(payload)
     .setProtectedHeader({ alg: "dir", enc: "A256GCM" })
     .setIssuedAt()
-    .setExpirationTime("3d") // 3 days
+    .setExpirationTime("3d")
     .encrypt(secret);
 
-  console.log("From JWE: Call DONE");
+  console.log("call Done from Create Token ");
   return jwe;
 }
 
--- Backend/Middleware/VerifyToken.js
+++ Backend/Middleware/VerifyToken.js
@@ -2,23 +2,25 @@ const { jwtDecrypt } = require("jose");
 const encoder = new TextEncoder();
 
 async function VerifyToken(req, res, next) {
+  console.log("call in Verify Token ");
   try {
-    console.log("VerifyToken Middleware: Start");
     const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res.status(401).json({ message: "No token provided" });
     }
 
     const token = authHeader.split(" ")[1];
-    const raw = process.env.SECRET_KEY.slice(0, 32);
+    const raw = process.env.SECRET_KEY.slice(0, 32); // must match CreateToken
     const secret = encoder.encode(raw);
 
     const { payload } = await jwtDecrypt(token, secret);
 
-    req.user = payload; // token info in req.user for use in controllers
-    next(); //call next middleware or route handler
-    console.log("VerifyToken Middleware: Done");
+    req.user = payload;
+    console.log("Decoded Payload:", payload); //  full payload
+
+    next();
   } catch (err) {
+    console.error("Token verification failed:", err.message);
     return res.status(401).json({ message: "Invalid or expired token" });
   }
 }
